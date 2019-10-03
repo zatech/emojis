e57791faf4078bbd576c08c16e5a953add3b7736
Nic <nicmalan@gmail.com>
2019-10-03 14:33:40 +0200

    Merges WIP into Master (#2)

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# emojis
+# ZATech custom emojis repository
+
+This is a holding place for all our custom emoji, and a little script to sync.
+
+If you'd like to have an emoji added, please create a pull request. Contributing guidelines coming soon :tm:
+
+```bash
+npm install
+node index.js xoxs-SLACK-OAUTH-TOKEN
+```
+
+# Guidelines to submitting emojis
+
+- emojis should be square and be able to be reduced to 128x128 px
+- emojis should have a transparent background
+- Images can only be JPG, GIF, or PNG (https://get.slack.help/hc/en-us/articles/206870177-Add-custom-emoji)
+- Maximum filesize is 128 KB, but we recommend 64kb or less
+- Please refrain from submitting controversial images such as hate speech symbols (including co-opted hate symbols), unauthorized profile images, etc
---
+++ index.js
@@ -0,0 +1,40 @@
+if (process.argv.length === 2) {
+    console.error('Pass in a Slack OAuth token as argument 1');
+    process.exit(1);
+}
+
+const emojme = require('emojme');
+const token = process.argv[2];
+const fs = require('fs');
+
+const downloadOptions = {
+    save: false,
+    bustCache: true,
+    output: false,
+};
+
+emojme.download('zatech', token, downloadOptions).then((res) => {
+    const remoteEmojis = res.zatech.emojiList.map(e => e.name);
+
+    const localEmojis = fs.readdirSync('./emojis/');
+    const missing = localEmojis.filter((fn) => {
+        const emojiName = removeExt(fn);
+        return remoteEmojis.indexOf(emojiName) === -1;
+    });
+
+    const addOptions = {
+        src: missing.map(e => './emojis/' + e), // File paths
+        name: missing.map(e => removeExt(e)), // Emoji names
+        bustCache: false,
+        avoidCollisions: false,
+        output: false,
+    };
+
+    emojme.add('zatech', token, addOptions).then((res) => {
+        console.log('Added Emoji:', res.zatech.emojiList);
+    });
+});
+
+function removeExt(string) {
+    return string.substr(0, string.indexOf('.'));
+}
